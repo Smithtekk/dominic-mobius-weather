e6d56385d1b227856c8da177f9243cd1eb8897c0
Dominic-Wassef <domwassef@gmail.com>
2022-06-19 22:26:32 -0400

    chore: implementing search / suggestions component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Footer from '../components/Footer/Footer';
+import Forecast from '../components/Forecast/Forecast';
 import Header from '../components/Header/Header';
+import Search from '../components/Search/Search';
 import Spinner from '../components/ui/Spinner/Spinner';
 import CurrentWeather from '../components/CurrentWeather/CurrentWeather';
 import { AppStore } from '../store/store';
@@ -15,7 +17,9 @@ const Home = () => {
     <>
       {loading && <Spinner />}
       <Header />
+      <Search />
       <CurrentWeather />
+      <Forecast />
       <Footer />
     </>
   );
